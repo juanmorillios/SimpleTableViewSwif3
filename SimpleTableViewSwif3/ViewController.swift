0981be1b246373b5e81112ff90f9a72a29eb3717
Juan Morillo <juanmorillios@gmail.com>
2016-08-15 22:40:55 +0200

    Initial Commit

---
+++ SimpleTableViewSwif3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTableViewSwif3
+//
+//  Created by Juan Morillo on 15/8/16.
+//  Copyright © 2016 JuanMorillios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
